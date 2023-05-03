a2f7a0b443f210c11a17430c98a7879f3c36c22e
AngeloCaporaso <angelo.caporaso@pagopa.it>
2023-05-03 11:10:25 +0200

    feat: Add full test feature
    Co-authored-by: Andrea De Rinaldis <andrea-deri@users.noreply.github.com>

--- integration-test/src/step_definitions/support/steps.js
+++ integration-test/src/step_definitions/support/steps.js
@@ -30,39 +30,42 @@ setDefaultTimeout(120 * 1000);
 /* 
  *  'Given' precondition for health checks on various services. 
  */
-Given('GPD service running', () => executeHealthCheckForGPD()); // ok
-Given('APIConfig service running', () => executeHealthCheckForAPIConfig()); // ok
-Given('GPD Payments service running', () => executeHealthCheckForGPDPayments()); // ok
-Given('reporting analysis service running', () => executeHealthCheckForReportingAnalysis()); // ok
+Given('GPD service running', () => executeHealthCheckForGPD());
+Given('APIConfig service running', () => executeHealthCheckForAPIConfig());
+Given('GPD Payments service running', () => executeHealthCheckForGPDPayments());
+Given('reporting analysis service running', () => executeHealthCheckForReportingAnalysis());
 
 
 /* 
  *  'Given' precondition for validating the entities to be used. 
  */
-Given('a not paid debt position', () => generateDebtPosition(bundle, true)); // delete
-Given('a paid debt position', () => generateAndPayDebtPosition(bundle)); // ok
-Given('a report flow sent to Node', () => sendReportFlowToNode(bundle)); // ok
+Given('a not paid debt position', () => generateDebtPosition(bundle, true));
+Given('a paid debt position', () => generateAndPayDebtPosition(bundle));
+Given('a report flow sent to Node', () => sendReportFlowToNode(bundle));
 
 
 /* 
  *  'When' clauses for executing actions.
  */
-When('the reporting batch analyzes the reporting flows for the organization', () => forceReportingBatchStart(bundle)); // ok
-When('the client waits its execution', () => waitWholeReportingProcessExecution()); // ok
+When('the reporting batch analyzes the reporting flows for the organization', () => forceReportingBatchStart(bundle));
+When('the client waits its execution', () => waitWholeReportingProcessExecution());
 
 
 /* 
  *  'Then' clauses for executing subsequential actions
  */
-Then('the client asks the flow list for the organization', () => retrieveReportFlowList(bundle)); // ok
-Then('the client asks the detail for one of the report flows', () => retrieveReportFlow(bundle)); // delete
+Then('the client asks the flow list for the organization', () => retrieveReportFlowList(bundle));
+Then('the client asks the detail for one of the report flows', () => retrieveReportFlow(bundle));
+Then('the client asks the detail for the analyzed debt positions', () => retrievePaymentOptionDetail(bundle));
 
 /* 
  *  'Then' clauses for assering retrieved data 
  */
-Then('the client receives status code {int}', (statusCode) => assertStatusCode(bundle.response, statusCode)); // ok
-Then('the client receives a non-empty list of flows', () => assertNonEmptyList(bundle.response)); // ok
-Then('the client receives an empty list of flows', () => assertEmptyList(bundle.response)); // ok
+Then('the client receives status code {int}', (statusCode) => assertStatusCode(bundle.response, statusCode));
+Then('the client receives a non-empty list of flows', () => assertNonEmptyList(bundle.response));
+Then('the client receives an empty list of flows', () => assertEmptyList(bundle.response));
+Then('the client receives the flow XML content', () => assertFlowXMLContent(bundle.response, bundle.flow.id));
+Then('the client receives the payment options with status {string}', (status) => assertPaymentOptionStatus(bundle.response, status));
 
 Before(function(scenario) {
     const header = `| Starting scenario "${scenario.pickle.name}" |`;
