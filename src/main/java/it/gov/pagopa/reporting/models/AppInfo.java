7296700c5b685452881f5f640e7e3aedbe3a3515
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-07-07 15:31:27 +0200

    [PAGOPA-1075] fix: removed unused annotated methods

--- src/main/java/it/gov/pagopa/reporting/models/AppInfo.java
+++ src/main/java/it/gov/pagopa/reporting/models/AppInfo.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
-@Data
 @Builder(toBuilder = true)
-@NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
