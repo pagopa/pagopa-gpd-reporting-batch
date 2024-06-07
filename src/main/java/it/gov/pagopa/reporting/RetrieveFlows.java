f4db1ad56683e0e2909be6727cf6cca86731cb95
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2024-06-07 11:21:48 +0200

    PAGOPA-1745 adding dynamic mechanism to retrieve station and broker

--- src/main/java/it/gov/pagopa/reporting/RetrieveFlows.java
+++ src/main/java/it/gov/pagopa/reporting/RetrieveFlows.java
@@ -6,15 +6,24 @@ import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.QueueTrigger;
 import com.sun.xml.ws.client.ClientTransportException;
+import it.gov.pagopa.reporting.client.ApiConfigClient;
+import it.gov.pagopa.reporting.exception.Cache4XXException;
+import it.gov.pagopa.reporting.exception.Cache5XXException;
 import it.gov.pagopa.reporting.models.OrganizationsMessage;
+import it.gov.pagopa.reporting.models.cache.CacheResponse;
+import it.gov.pagopa.reporting.models.cache.CreditorInstitutionStation;
+import it.gov.pagopa.reporting.models.cache.Station;
 import it.gov.pagopa.reporting.service.FlowsService;
 import it.gov.pagopa.reporting.service.NodoChiediElencoFlussi;
 import it.gov.pagopa.reporting.service.OrganizationsService;
 import it.gov.pagopa.reporting.servicewsdl.FaultBean;
 import it.gov.pagopa.reporting.servicewsdl.TipoElencoFlussiRendicontazione;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,6 +41,8 @@ public class RetrieveFlows {
     private final String initialVisibilityDelayInSeconds = System.getenv("QUEUE_DELAY_SEC");
     private final String maxRetryQueuing = System.getenv("MAX_RETRY_QUEUING");
 
+    private static CacheResponse cacheContent;
+
     /**
      * This function will be invoked when a new message is detected in the queue
      */
@@ -45,6 +56,10 @@ public class RetrieveFlows {
 
         NodoChiediElencoFlussi nodeClient = this.getNodeClientInstance(logger);
         FlowsService flowsService = this.getFlowsServiceInstance(logger);
+        ApiConfigClient cacheClient = ApiConfigClient.getInstance();
+        if(cacheContent == null || (cacheContent.getRetrieveDate() != null && cacheContent.getRetrieveDate().isBefore(LocalDateTime.now()))) {
+            setCache(cacheClient, logger);
+        }
 
         try {
             OrganizationsMessage organizationsMessage = new ObjectMapper().readValue(message, OrganizationsMessage.class);
@@ -52,8 +67,12 @@ public class RetrieveFlows {
             Arrays.stream(organizationsMessage.getIdPA())
                     .forEach((organization -> {
                         try {
+                            Station stationBroker = getPAStationIntermediario(organization)
+                                    .orElseThrow(() -> new RuntimeException(String.format("No data present in api config database for PA %s", organization)));
+                            String idStation = stationBroker.getStationCode();
+                            String idBroker = stationBroker.getBrokerCode();
                             // call NODO dei pagamenti
-                            nodeClient.nodoChiediElencoFlussiRendicontazione(organization);
+                            nodeClient.nodoChiediElencoFlussiRendicontazione(organization, idBroker, idStation);
 
                             // retrieve result
                             FaultBean faultBean = nodeClient.getNodoChiediElencoFlussiRendicontazioneFault();
@@ -93,5 +112,35 @@ public class RetrieveFlows {
 
     public OrganizationsService getOrganizationsServiceInstance(Logger logger) {
         return new OrganizationsService(this.storageConnectionString, this.organizationsTable, this.organizationsQueue, Integer.parseInt(timeToLiveInSeconds), Integer.parseInt(initialVisibilityDelayInSeconds), logger);
+    }
+
+    public Optional<Station> getPAStationIntermediario(String idPa) {
+        List<String> stationPa = getStazioni(idPa);
+        return cacheContent.getStations().stream()
+                .filter(station -> stationPa.contains(station.getStationCode()))
+                .filter(Station::getEnabled)
+                .findFirst();
+    }
+
+    public List<String> getStazioni(String idPa) {
+        return cacheContent.getCreditorInstitutionStations().stream()
+                .filter(creditorInstitutionStation -> creditorInstitutionStation.getCreditorInstitutionCode().equals(idPa))
+                .map(CreditorInstitutionStation::getStationCode).toList();
+    }
+
+    public synchronized void setCache(ApiConfigClient cacheClient, Logger logger) {
+        try {
+            if(cacheContent == null) {
+                cacheContent = cacheClient.getCache();
+                cacheContent.setRetrieveDate(LocalDateTime.now());
+            }
+        } catch (Cache4XXException | Cache5XXException e) {
+            cacheContent = null;
+            logger.log(Level.SEVERE, e.getMessage());
+        } catch (IOException e) {
+            cacheContent = null;
+            logger.log(Level.SEVERE, e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
