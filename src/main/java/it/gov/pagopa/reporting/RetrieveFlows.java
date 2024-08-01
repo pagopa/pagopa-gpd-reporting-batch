42318b1591bc5f088e4671b48f2ee3a83addec16
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2024-08-01 11:03:03 +0200

    PAGOPA-2012 removing null clause from log

--- src/main/java/it/gov/pagopa/reporting/RetrieveFlows.java
+++ src/main/java/it/gov/pagopa/reporting/RetrieveFlows.java
@@ -102,7 +102,7 @@ public class RetrieveFlows {
                                 logger.log(Level.SEVERE, () -> "[NODO Connection down]  Max retry exceeded.");
                             }
                         } catch (AppException e) {
-                            logger.log(Level.SEVERE, () -> "[CACHE] Organization: [" + organization +"] Caused by: " + e.getCause() + " Message: " + e.getMessage());
+                            logger.log(Level.SEVERE, () -> "[CACHE] Organization: [" + organization + "] Message: " + e.getMessage());
                         }
 
                     }));
